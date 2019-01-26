a40384edbb21a980fcc632151cad203f455204c7
LeoCal4 <leonardo.calamita@studenti.unicam.it>
2019-01-26 15:31:30 +0100

    Fix round up doubles

--- src/main/java/bpmnMetadataExtractor/JsonEncoder.java
+++ src/main/java/bpmnMetadataExtractor/JsonEncoder.java
@@ -111,6 +111,7 @@ public class JsonEncoder {
 		String metricInfos[] = metricsInfos.get(metricName);
 		if (!Double.isFinite(n))
 			n = 0;
+		n = Math.round(n * 1000.0) / 1000.0;
 		advMetric.put("value", n);
 		advMetric.put("description", metricInfos[0]);
 		advMetric.put("source", metricInfos[1]);
